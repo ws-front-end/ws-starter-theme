593cdfc6dc6ff2e87a9a3da390fb86a296094d4e
Jõeleht <joosep.joeleht@gmail.com>
2019-03-07 10:33:10 +0200

    Added build script
    Refer to wiki for more information.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,168 +2,203 @@
  * Gulpfile.
  *
  */
-const gulp = require('gulp');
-const newer = require('gulp-newer');
-const plumber = require('gulp-plumber');
-const imagemin = require('gulp-imagemin');
-const rename = require('gulp-rename');
-const sass = require('gulp-sass');
-const sourcemaps = require('gulp-sourcemaps');
-const postcss = require('gulp-postcss');
-const path = require('path');
-const webpack = require('webpack-stream');
-const minifyPlugin = require('babel-minify-webpack-plugin');
-const browsersync = require('browser-sync');
+const gulp = require('gulp')
+const newer = require('gulp-newer')
+const plumber = require('gulp-plumber')
+const imagemin = require('gulp-imagemin')
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')
+const sourcemaps = require('gulp-sourcemaps')
+const postcss = require('gulp-postcss')
+const postcssAssets = require('postcss-assets')
+const path = require('path')
+const webpack = require('webpack-stream')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const browsersync = require('browser-sync')
+const autoprefixer = require('autoprefixer')
+const cssMqpacker = require('css-mqpacker')
+const cssnano = require('cssnano')
 
 /**
  * Configuration.
  */
-('use strict');
-const projectUrlName = 'Gulpfile-35';
-const projectUrl = `http://localhost/${projectUrlName}`;
-// const projectUrl = `${projectUrlName}.test`;
-const projectName = path.basename(__dirname),
-	dir = {
-		src: './',
-		build: './',
-	},
-	browserSyncOptions = {
-		proxy: projectUrl,
-		open: true,
-		notify: true,
-		ghostMode: false,
-		ui: {
-			port: 8001,
-		},
-	},
-	php = {
-		src: dir.src + '**/*.php',
-		build: dir.build,
-	},
-	images = {
-		src: dir.src + 'assets/src/img/**/*',
-		build: dir.build + 'assets/dist/img/',
-	},
-	css = {
-		src: dir.src + 'assets/src/sass/main.scss',
-		watch: dir.src + 'assets/src/sass/**/*',
-		build: dir.build,
-		sassOpts: {
-			outputStyle: 'compressed',
-			imagePath: images.build,
-			precision: 3,
-			errLogToConsole: true,
-		},
-		processors: [
-			require('postcss-assets')({
-				loadPaths: ['assets/dist/img/', 'assets/dist/img/svg'],
-				basePath: dir.build,
-				baseUrl: '/wp-content/themes/' + projectName + '/',
-			}),
-			require('autoprefixer'),
-			require('css-mqpacker'),
-			require('cssnano'),
-		],
-	},
-	js = {
-		src: dir.src + 'assets/src/js/app.js',
-		watch: dir.src + 'assets/src/js/**/*',
-		build: dir.build + 'assets/dist/js/',
-		filename: 'bundled-scripts.js',
-	},
-	browserSyncServer = browsersync.create();
+const projectUrlName = 'starter'
+// const projectUrl = `http://localhost/${projectUrlName}`
+const projectUrl = `${projectUrlName}.test`
+const projectName = path.basename(__dirname)
+const dir = {
+  src: './',
+  build: './',
+}
+const browserSyncOptions = {
+  proxy: projectUrl,
+  open: true,
+  notify: true,
+  ghostMode: false,
+  ui: {
+    port: 8001,
+  },
+}
+const php = {
+  src: `${dir.src}**/*.php`,
+  build: dir.build,
+}
+const images = {
+  src: `${dir.src}assets/src/img/**/*`,
+  build: `${dir.build}assets/dist/img/`,
+}
+let css = {
+  src: `${dir.src}assets/src/sass/main.scss`,
+  watch: `${dir.src}assets/src/sass/**/*`,
+  build: dir.build,
+  sassOpts: {
+    outputStyle: 'compressed',
+    imagePath: images.build,
+    precision: 3,
+    errLogToConsole: true,
+  },
+  processors: [
+    postcssAssets({
+      loadPaths: ['assets/dist/img/', 'assets/dist/img/svg'],
+      basePath: dir.build,
+      baseUrl: `/wp-content/themes/${projectName}/`,
+    }),
+    autoprefixer,
+    cssMqpacker,
+  ],
+}
+if (process.env.NODE_ENV === 'production') {
+  css = {
+    ...css,
+    ...{
+      processors: [...css.processors, cssnano],
+    },
+  }
+}
+const js = {
+  src: `${dir.src}assets/src/js/app.js`,
+  watch: `${dir.src}assets/src/js/**/*`,
+  build: `${dir.build}assets/dist/js/`,
+  filename: {
+    dev: 'bundle.min.js',
+    build: 'bundle.min.js',
+  },
+}
+let webPackConfig = {
+  entry: js.src,
+  watch: false,
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  externals: {
+    jquery: 'jQuery',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            exclude: /node_modules/,
+            babelrc: true,
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname), 'node_modules'],
+  },
+  output: {
+    filename: js.filename.dev,
+    path: path.resolve(js.build),
+  },
+}
+if (process.env.NODE_ENV === 'production') {
+  webPackConfig = {
+    ...webPackConfig,
+    ...{
+      watch: false,
+      mode: 'production',
+      optimization: {
+        minimizer: [
+          new UglifyJsPlugin({
+            uglifyOptions: {
+              parallel: true,
+              output: {
+                comments: false,
+              },
+              toplevel: true,
+            },
+          }),
+        ],
+      },
+    },
+  }
+  delete webPackConfig.devtool
+}
+const browserSyncServer = browsersync.create()
 
 const imagesTask = () => {
-	return gulp
-		.src(images.src)
-		.pipe(plumber())
-		.pipe(newer(images.build))
-		.pipe(imagemin())
-		.pipe(gulp.dest(images.build));
-};
+  return gulp
+    .src(images.src)
+    .pipe(plumber())
+    .pipe(newer(images.build))
+    .pipe(imagemin())
+    .pipe(gulp.dest(images.build))
+}
 const cssTask = () => {
-	return gulp
-		.src(css.src)
-		.pipe(plumber())
-		.pipe(sourcemaps.init())
-		.pipe(sass(css.sassOpts))
-		.pipe(postcss(css.processors))
-		.pipe(
-			rename({
-				basename: 'style',
-			})
-		)
-		.pipe(sourcemaps.write({ includeContent: false }))
-		.pipe(sourcemaps.write(css.build))
-		.pipe(gulp.dest(css.build))
-		.pipe(browserSyncServer.stream());
-};
+  let $retVal = gulp.src(css.src).pipe(plumber())
+
+  if (process.env.NODE_ENV !== 'production') {
+    $retVal = $retVal.pipe(sourcemaps.init())
+  }
+  $retVal = $retVal
+    .pipe(sass(css.sassOpts))
+    .pipe(postcss(css.processors))
+    .pipe(
+      rename({
+        basename: 'style',
+      })
+    )
+  if (process.env.NODE_ENV !== 'production') {
+    $retVal = $retVal.pipe(sourcemaps.write())
+  }
+  $retVal = $retVal.pipe(gulp.dest(css.build)).pipe(browserSyncServer.stream())
+  return $retVal
+}
+
 const jsTask = () => {
-	return gulp
-		.src(js.src)
-		.pipe(plumber())
-		.pipe(
-			webpack({
-				watch: true,
-				entry: js.src,
-				mode: 'production',
-				externals: {
-					jquery: 'jQuery',
-				},
-				module: {
-					rules: [
-						{
-							test: /\.js?$/,
-							use: {
-								loader: 'babel-loader',
-								options: {
-									exclude: /node_modules/,
-									babelrc: true,
-								},
-							},
-						},
-						{
-							test: /\.css$/,
-							use: ['style-loader', 'css-loader'],
-						},
-					],
-				},
-				plugins: [new minifyPlugin()],
-				resolve: {
-					modules: [path.resolve(__dirname), 'node_modules'],
-				},
-				output: {
-					filename: 'bundle.min.js',
-					path: path.resolve(js.build),
-				},
-				performance: {
-					hints: 'warning',
-				},
-			})
-		)
-		.pipe(gulp.dest(js.build))
-		.pipe(browserSyncServer.reload({ stream: true }));
-};
+  return gulp
+    .src(js.src)
+    .pipe(plumber())
+    .pipe(webpack(webPackConfig))
+    .pipe(gulp.dest(js.build))
+    .pipe(browserSyncServer.reload({ stream: true }))
+}
 
-const build = gulp.series(imagesTask, gulp.parallel(cssTask, jsTask));
-
+const development = gulp.series(imagesTask, gulp.parallel(cssTask, jsTask))
+const build = gulp.series(imagesTask, gulp.parallel(cssTask, jsTask))
 const serve = done => {
-	browserSyncServer.init(browserSyncOptions);
-	done();
-};
+  browserSyncServer.init(browserSyncOptions)
+  done()
+}
 const reload = done => {
-	browserSyncServer.reload();
-	done();
-};
-
+  browserSyncServer.reload()
+  done()
+}
 const watchEverything = () => {
-	gulp.watch(php.src, gulp.series(reload));
-	gulp.watch(images.src, gulp.series(imagesTask));
-	gulp.watch(css.watch, gulp.series(cssTask));
-	gulp.watch(js.watch, gulp.series(jsTask, reload));
-};
-const defaultTasks = gulp.parallel(build, gulp.series(serve, watchEverything));
+  gulp.watch(php.src, gulp.series(reload))
+  gulp.watch(images.src, gulp.series(imagesTask))
+  gulp.watch(css.watch, gulp.series(cssTask))
+  gulp.watch(js.watch, gulp.series(jsTask))
+}
+const defaultTask = gulp.parallel(development, gulp.series(serve, watchEverything))
 
-export { imagesTask, cssTask, jsTask, watchEverything };
+export { imagesTask, cssTask, jsTask, watchEverything, build }
 
-export default defaultTasks;
+export default defaultTask

f9d01999e88a35e8a03cdb0dfad546dac9bc2301
jissepo <jissepo@users.noreply.github.com>
2019-07-05 16:38:27 +0300

    Create cf7-disable-fix.js

---
+++ assets/src/js/components/cf7-disable-fix.js
@@ -0,0 +1,30 @@
+class CF7_disabler {
+    constructor() {
+      // Start of js
+      this.init()
+    }
+
+    init() {
+      setTimeout(() => {
+        document.querySelectorAll('.wpcf7-form input[type="submit"]').forEach(function(el) {
+          el.removeAttribute('disabled')
+          const container = el.closest('.wpcf7')
+          container.addEventListener(
+            'wpcf7invalid',
+            () => {
+              container.querySelectorAll('input[name="your-consent"]').forEach(el2 => {
+                if (!el2.checked) {
+                  container.classList.add('red-checkbox')
+                } else {
+                  container.classList.remove('red-checkbox')
+                }
+              })
+            },
+            false
+          )
+        })
+      }, 1000)
+    }
+  }
+
+  new CF7_disabler()

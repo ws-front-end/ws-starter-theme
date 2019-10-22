294e79663222ea6b479555a86f31aeb3db5180f7
andresaareli <30658966+andresaareli@users.noreply.github.com>
2019-10-22 15:00:38 +0300

    Update title html h2 to h1

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,7 +9,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="page-heading">
-		<h2><?php the_title(); ?></h2>
+		<h1><?php the_title(); ?></h1>
 	</div>
 
 	<div class="entry-content">

10321e93427deee9cb8d745511753f6eafa64673
Joosep Jõeleht <joosep.joeleht@gmail.com>
2020-02-17 15:30:08 +0200

    Admin controller and styles

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
@@ -8,15 +8,18 @@ require_once 'includes/classes/class-ws-relative-url-maker.php';
 require_once 'includes/classes/class-ws-woocommerce-functions.php';
 require_once 'includes/classes/class-ws-ajax-functions.php';
 require_once 'includes/classes/class-admin-post-functions.php';
+require_once 'includes/classes/class-ws-admin-controller.php';
 
 /**
  * Class ThemeSetup
  */
-class ThemeSetup {
+class ThemeSetup
+{
 	/**
 	 * ThemeSetup constructor.
 	 */
-	public function __construct() {
+	public function __construct()
+	{
 		$this->remove_actions();
 		$this->remove_filters();
 		$this->add_actions();
@@ -26,7 +29,8 @@ class ThemeSetup {
 		new WS_Relative_Url_Maker();
 		new WS_Admin_Post_Functions();
 		new WS_Ajax_Functions();
-		if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true ) ) {
+		new WS_Admin_Controller();
+		if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')), true)) {
 			new WS_Woocommerce_Functions();
 		}
 	}
@@ -34,94 +38,121 @@ class ThemeSetup {
 	/**
 	 * Add WordPress actions
 	 */
-	private function add_actions() {
-		add_action( 'wp_head', 'ob_start', 1, 0 );
-		add_action( 'after_setup_theme', [ $this, 'ws_theme_setup' ] );
-		add_action( 'wp_enqueue_scripts', [ $this, 'ws_theme_enqueue_scripts_and_styles' ] );
-		add_action( 'the_generator', '__return_empty_string' );
-		add_action( 'widgets_init', [ $this, 'theme_widgets_sidebar_register' ] );
-		add_action( 'wp_before_admin_bar_render', [ $this, 'add_admin_bar_button' ] );
-		add_action( 'admin_init', [ $this, 'add_site_options_menu_item' ] );
-		add_action( 'admin_init', [ $this, 'hide_content_editor_from_options_page' ] );
-		add_action( 'admin_menu', [ $this, 'hide_page_attributes_from_options_page' ] );
+	private function add_actions()
+	{
+		add_action('wp_head', 'ob_start', 1, 0);
+		add_action('after_setup_theme', [$this, 'ws_theme_setup']);
+		add_action('wp_enqueue_scripts', [$this, 'ws_theme_enqueue_scripts_and_styles']);
+		add_action('the_generator', '__return_empty_string');
+		add_action('widgets_init', [$this, 'theme_widgets_sidebar_register']);
+		add_filter('ws_get_url_from_acf_image_array', [$this, 'ws_get_url_from_acf_image_array'], 10, 3);
 	}
 
 	/**
 	 * Remove WordPress existing filters
 	 */
-	private function remove_filters() {
+	private function remove_filters()
+	{
 		// remove_filter('filter_name', 'function_name', 'priority); !
+
 	}
 
 	/**
 	 * Remove WordPress existing actions
 	 */
-	private function remove_actions() {
-		remove_action( 'wp_head', 'feed_links_extra', 3 );
-		remove_action( 'wp_head', 'rsd_link' );
-		remove_action( 'wp_head', 'wlwmanifest_link' );
-		remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10 );
-		remove_action( 'wp_head', 'wp_generator' );
-		remove_action( 'wp_head', 'wp_shortlink_wp_head', 10 );
-		remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-		remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
-		remove_action( 'wp_head', 'wp_oembed_add_host_js' );
-		remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
-		remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-		remove_action( 'wp_print_styles', 'print_emoji_styles' );
-		remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-		remove_action( 'admin_print_styles', 'print_emoji_styles' );
-		remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-		remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	private function remove_actions()
+	{
+		remove_action('wp_head', 'feed_links_extra', 3);
+		remove_action('wp_head', 'rsd_link');
+		remove_action('wp_head', 'wlwmanifest_link');
+		remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10);
+		remove_action('wp_head', 'wp_generator');
+		remove_action('wp_head', 'wp_shortlink_wp_head', 10);
+		remove_action('wp_head', 'print_emoji_detection_script', 7);
+		remove_action('wp_head', 'wp_oembed_add_discovery_links');
+		remove_action('wp_head', 'wp_oembed_add_host_js');
+		remove_action('wp_head', 'rest_output_link_wp_head', 10);
+		remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+		remove_action('wp_print_styles', 'print_emoji_styles');
+		remove_action('admin_print_scripts', 'print_emoji_detection_script');
+		remove_action('admin_print_styles', 'print_emoji_styles');
+		remove_filter('the_content_feed', 'wp_staticize_emoji');
+		remove_filter('comment_text_rss', 'wp_staticize_emoji');
+
+		remove_action('login_head', 'wp_shake_js', 12);
 	}
 
 	/**
 	 * Add WordPress custom filters
 	 */
-	private function add_filters() {
-		add_filter( 'body_class', [ $this, 'ws_body_class' ] );
-		add_filter( 'use_default_gallery_style', '__return_false' );
-		add_filter( 'emoji_svg_url', '__return_false' );
-		add_filter( 'show_recent_comments_widget_style', '__return_false' );
-		add_filter( 'parse_query', [ $this, 'hide_options_page' ] );
+	private function add_filters()
+	{
+		add_filter('body_class', [$this, 'ws_body_class']);
+		add_filter('use_default_gallery_style', '__return_false');
+		add_filter('emoji_svg_url', '__return_false');
+		add_filter('show_recent_comments_widget_style', '__return_false');
 	}
 
 	/**
 	 * Setup various WordPress theme variables
 	 */
-	public function ws_theme_setup() {
+	public function ws_theme_setup()
+	{
 		// Make theme available for translation.
-		load_theme_textdomain( 'ws-starter-theme', get_template_directory() . '/languages' );
+		load_theme_textdomain('ws-starter-theme', get_template_directory() . '/languages');
 
-		add_image_size( '1440p', 2560, 1440 );
-		add_image_size( '4k', 4096, 2160 );
+		add_image_size('1440p', 2560, 1440);
+		add_image_size('4k', 4096, 2160);
 
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+		add_theme_support('automatic-feed-links');
 
 		// Let WordPress manage the document title.
-		add_theme_support( 'title-tag' );
+		add_theme_support('title-tag');
 
 		// Enable support for Post Thumbnails on posts and pages.
-		add_theme_support( 'post-thumbnails' );
+		add_theme_support('post-thumbnails');
 
 		// Switch default core markup for search form, comment form, and comments.
 		add_theme_support(
 			'html5',
-			[
+			array(
 				'search-form',
 				'comment-form',
 				'comment-list',
 				'gallery',
 				'caption',
-			]
+			)
 		);
 
 		register_nav_menus(
+			array(
+				'header-menu' => esc_html__('Header', 'ws-starter-theme'),
+			)
+		);
+	}
+
+	/** Enqueues theme stylesheet and Javascript files.
+	 */
+	public function ws_theme_enqueue_scripts_and_styles()
+	{
+		$manifest      = json_decode(file_get_contents(get_template_directory() . '/package.json', true));
+		$asset_version = $manifest->version;
+
+		wp_enqueue_style('ws-main-stylesheet', get_stylesheet_uri(), [], $asset_version);
+
+		wp_enqueue_script('ws-custom-js', get_template_directory_uri() . '/assets/dist/js/bundle.min.js', array('jquery'), $asset_version, true);
+		wp_localize_script(
+			'ws-custom-js',
+			'php_object',
 			[
-				'header-menu' => esc_html__( 'Header', 'ws-starter-theme' ),
+				'ajax_url' => admin_url('admin-ajax.php'),
 			]
 		);
+
+		if (is_singular() && comments_open() && get_option('thread_comments')) {
+			wp_enqueue_script('comment-reply');
+		}
 	}
 
 	/** Adds a certain class to body tag at specified situations.
@@ -130,68 +161,40 @@ class ThemeSetup {
 	 *
 	 * @return array
 	 */
-	public function ws_body_class( $classes ) {
+	public function ws_body_class($classes)
+	{
 		// Adds a class of hfeed to non-singular pages.
-		if ( ! is_singular() ) {
+		if (!is_singular()) {
 			$classes[] = 'hfeed';
 		}
 
 		return $classes;
 	}
 
-	/** Enqueues theme stylesheet and Javascript files.
-	 */
-	public function ws_theme_enqueue_scripts_and_styles() {
-		$manifest      = json_decode( file_get_contents( get_template_directory() . '/package.json', true ) );
-		$asset_version = $manifest->version;
-
-		wp_enqueue_style( 'ws-main-stylesheet', get_stylesheet_uri(), [], $asset_version );
-
-		wp_enqueue_script( 'ws-custom-js', get_template_directory_uri() . '/assets/dist/js/bundle.min.js', [ 'jquery' ], $asset_version, true );
-		wp_localize_script(
-			'ws-custom-js',
-			'php_object',
-			[
-				'ajax_url' => admin_url( 'admin-ajax.php' ),
-			]
-		);
-
-		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-			wp_enqueue_script( 'comment-reply' );
-		}
-	}
 
 	/**
 	 * Registers a WordPress sidebar
 	 */
-	public function ws_head_pingbacks() {
-		if ( is_singular() && pings_open() ) {
-			echo '<link rel="pingback" href="', esc_url( get_bloginfo( 'pingback_url' ) ), '">';
+	public function ws_head_pingbacks()
+	{
+		if (is_singular() && pings_open()) {
+			echo '<link rel="pingback" href="', esc_url(get_bloginfo('pingback_url')), '">';
 		}
 	}
 
-	/**
-	 * Registers sidebars for widgets.
-	 */
-	public function theme_widgets_sidebar_register() {
+	public function theme_widgets_sidebar_register()
+	{
 		register_sidebar(
-			[
-				'name'          => __( 'Main Sidebar' ),
+			array(
+				'name'          => __('Main Sidebar'),
 				'id'            => 'main-sidebar',
-				'description'   => __( 'Widgets in this area can be called out with ' ),
+				'description'   => __('Widgets in this area can be called out with '),
 				'before_widget' => '<div id="%1$s" class="widget %2$s">',
 				'after_widget'  => '</div>',
 				'before_title'  => '<h3 class="widget-title">',
 				'after_title'   => '</h3>',
-			]
+			)
 		);
-	}
-
-	/**
-	 * Adds custom admin bar button.
-	 */
-	public static function add_admin_bar_button() {
-		get_template_part( 'template-parts/adminbar-button' );
 	}
 
 	/**
@@ -199,11 +202,12 @@ class ThemeSetup {
 	 *
 	 * @return int|WP_Error
 	 */
-	public static function get_theme_options_page_id() {
-		$ws_options_page_id = wp_cache_get( 'WS_OPTIONS_PAGE_ID' );
-		if ( false === $ws_options_page_id ) {
+	public static function get_theme_options_page_id()
+	{
+		$ws_options_page_id = wp_cache_get('WS_OPTIONS_PAGE_ID');
+		if (false === $ws_options_page_id) {
 			$args  = [
-				'post_status'						=> [ 'publish', 'private' ],
+				'post_status'						=> ['publish', 'private'],
 				'post_type'                         => 'page',
 				'fields'                            => 'ids',
 				'nopaging'                          => true,
@@ -212,100 +216,19 @@ class ThemeSetup {
 				'numberposts'                       => 1,
 				'disable_for_options_page_id_query' => 'yes',
 			];
-			$pages = get_posts( $args );
-			if ( ! empty( $pages ) ) {
+			$pages = get_posts($args);
+			if (!empty($pages)) {
 				$ws_options_page_id = $pages[0];
-				wp_cache_set( 'WS_OPTIONS_PAGE_ID', $ws_options_page_id );
-
-				return apply_filters( 'wpml_object_id', $ws_options_page_id, 'page', true );
+				wp_cache_set('WS_OPTIONS_PAGE_ID', $ws_options_page_id);
 			} else {
-				return new WP_Error( 'Unable to fetch options page ID. It might not exist?' );
+				return new WP_Error('Unable to fetch options page ID. It might not exist?');
 			}
 		}
 
-		return apply_filters( 'wpml_object_id', $ws_options_page_id, 'page', true );
+		return apply_filters('wpml_object_id', $ws_options_page_id, 'page', true);
 	}
 
-	/**
-	 * Hides options page from everywhere.
-	 *
-	 * @param WP_Query $query WordPress query instance.
-	 */
-	public function hide_options_page( $query ) {
-		global $pagenow, $post_type;
-		if ( is_admin() && 'edit.php' === $pagenow && 'page' === $post_type ) {
-			if ( ! filter_var( $query->get( 'disable_for_options_page_id_query' ), FILTER_VALIDATE_BOOLEAN ) ) {
-				$options_page_id = self::get_theme_options_page_id();
-				if ( ! is_wp_error( $options_page_id ) && filter_var( $options_page_id, FILTER_VALIDATE_INT ) ) {
-					if ( 'ws-general-options-page-dummy-template.php' !== $query->query_vars['meta_value'] ) {
-						$post_not_in = $query->query_vars['post__not_in'];
-						if ( ! is_array( $post_not_in ) ) {
-							$post_not_in = [];
-						}
-						$post_not_in[] = $options_page_id;
 
-						$query->query_vars['post__not_in'] = $post_not_in;
-					}
-				}
-			}
-		}
-	}
-
-	/**
-	 * Hides the content editor from the options page admin UI.
-	 */
-	public function hide_content_editor_from_options_page() {
-		$post_id = filter_input( INPUT_GET, 'post', FILTER_VALIDATE_INT );
-		if ( ! $post_id ) {
-			$post_id = filter_input( INPUT_POST, 'post', FILTER_VALIDATE_INT );
-		}
-		if ( ! $post_id ) {
-			return;
-		}
-		$options_page_id = self::get_theme_options_page_id();
-		if ( ! is_wp_error( $options_page_id ) && filter_var( $options_page_id, FILTER_VALIDATE_INT ) ) {
-			if ( $options_page_id === $post_id ) {
-				remove_post_type_support( 'page', 'editor' );
-				remove_post_type_support( 'page', 'thumbnail' );
-			}
-		}
-	}
-	/**
-	 * Hides the page attributes from the options page admin UI.
-	 */
-	public function hide_page_attributes_from_options_page() {
-		$post_id = filter_input( INPUT_GET, 'post', FILTER_VALIDATE_INT );
-		if ( ! $post_id ) {
-			$post_id = filter_input( INPUT_POST, 'post', FILTER_VALIDATE_INT );
-		}
-		if ( ! $post_id ) {
-			return;
-		}
-		$options_page_id = self::get_theme_options_page_id();
-		if ( ! is_wp_error( $options_page_id ) && filter_var( $options_page_id, FILTER_VALIDATE_INT ) ) {
-			if ( $options_page_id === $post_id ) {
-				remove_meta_box( 'pageparentdiv', 'page', 'normal' );
-			}
-		}
-	}
-
-	/**
-	 * Adds the options page link to the admin menu.
-	 */
-	public function add_site_options_menu_item() {
-		$options_page_id = self::get_theme_options_page_id();
-		if ( ! is_wp_error( $options_page_id ) && filter_var( $options_page_id, FILTER_VALIDATE_INT ) ) {
-			add_menu_page(
-				'ws_site_options',
-				esc_html__( 'Site options' ),
-				'read',
-				'post.php?post=' . $options_page_id . '&action=edit',
-				'',
-				'dashicons-admin-site',
-				12
-			);
-		}
-	}
 
 	/**
 	 * Gets the theme option value for the page id or current page
@@ -315,25 +238,53 @@ class ThemeSetup {
 	 *
 	 * @return string|int|array
 	 */
-	public static function get_theme_option( string $meta_key, int $post_id = 0 ) {
-		if ( ! function_exists( 'get_field' ) ) {
+	public static function get_theme_option(string $meta_key, int $post_id = 0)
+	{
+		if (!function_exists('get_field')) {
 			return null;
 		}
 
-		if ( 0 === $post_id ) {
-			$post_id = get_the_id();
-		}
-		if ( class_exists( 'SitePress' ) ) {
-			$post_id = apply_filters( 'wpml_object_id', $post_id, 'page', true );
-		}
-		$meta_value = get_field( $meta_key, $post_id );
-		if ( $meta_value ) {
-			return $meta_value;
+		if (0 !== $post_id) {
+			// $post_id = get_the_id();
+			// if (class_exists('SitePress')) {
+			// 	$post_id = apply_filters('wpml_object_id', $post_id, 'page', true);
+			// }
+			$meta_value = get_field($meta_key, $post_id);
+			if ($meta_value) {
+				return $meta_value;
+			} else {
+				return false;
+			}
 		}
 
-		return get_field( $meta_key, self::get_theme_options_page_id() );
+		return get_field($meta_key, self::get_theme_options_page_id());
 	}
 
+	/** Get any field from ACF image array. Also able to get certain size only if image URL is asked for.
+	 *
+	 * @param array  $image_array Image array from ACF image field.
+	 * @param string $field Name of the field required from the image array.
+	 * @param string $size Image size slug.
+	 *
+	 * @return bool|mixed Returns false if field or image size not found. Otherwise returns the requested value.
+	 */
+	public function ws_get_url_from_acf_image_array($image_array = [], $field = 'url', $size = 'medium')
+	{
+		if (!is_array($image_array) || empty($image_array)) {
+			return false;
+		}
+		if ('url' === $field || 'sizes' === $field) {
+			if (isset($image_array['sizes'][$size])) {
+				return $image_array['sizes'][$size];
+			}
+		} else {
+			if (isset($image_array[$field])) {
+				return $image_array[$field];
+			}
+		}
+
+		return false;
+	}
 
 	/**
 	 * Generate img tag with various attributes.
@@ -343,8 +294,9 @@ class ThemeSetup {
 	 *
 	 * @return string
 	 */
-	public function generate_img_html( $image_id = false, $args = [] ) {
-		if ( ! $image_id ) {
+	public function generate_img_html($image_id = false, $args = [])
+	{
+		if (!$image_id) {
 			return '';
 		}
 
@@ -357,25 +309,25 @@ class ThemeSetup {
 			'title'            => '',
 		];
 
-		$options = wp_parse_args( $args, $default_args );
+		$options = wp_parse_args($args, $default_args);
 
 		$img_html = '<img ';
 
-		if ( $options['disable_lazyload'] ) {
-			$img_html .= 'src="' . esc_url( wp_get_attachment_image_url( $image_id, $options['size'] ) ) . '" ';
-			$img_html .= 'class="' . esc_attr( implode( ' ', $options['classes'] ) ) . '" ';
-			$img_html .= 'srcset="' . esc_attr( wp_get_attachment_image_srcset( $image_id, $options['size'] ) ) . '" ';
-			$img_html .= 'sizes="' . esc_attr( wp_get_attachment_image_sizes( $image_id, $options['size'] ) ) . '"';
+		if ($options['disable_lazyload']) {
+			$img_html .= 'src="' . esc_url(wp_get_attachment_image_url($image_id, $options['size'])) . '" ';
+			$img_html .= 'class="' . esc_attr(implode(' ', $options['classes'])) . '" ';
+			$img_html .= 'srcset="' . esc_attr(wp_get_attachment_image_srcset($image_id, $options['size'])) . '" ';
+			$img_html .= 'sizes="' . esc_attr(wp_get_attachment_image_sizes($image_id, $options['size'])) . '"';
 		} else {
-			$img_html .= 'data-src="' . esc_url( wp_get_attachment_image_url( $image_id, $options['size'] ) ) . '" ';
-			$img_html .= 'data-srcset="' . esc_attr( wp_get_attachment_image_srcset( $image_id, $options['size'] ) ) . '" ';
-			$img_html .= 'data-sizes="' . esc_attr( wp_get_attachment_image_sizes( $image_id, $options['size'] ) ) . '"';
+			$img_html .= 'data-src="' . esc_url(wp_get_attachment_image_url($image_id, $options['size'])) . '" ';
+			$img_html .= 'data-srcset="' . esc_attr(wp_get_attachment_image_srcset($image_id, $options['size'])) . '" ';
+			$img_html .= 'data-sizes="' . esc_attr(wp_get_attachment_image_sizes($image_id, $options['size'])) . '"';
 
-			if ( $options['for_swiper'] ) {
-				$img_html .= 'class="swiper-lazy ' . esc_attr( implode( ' ', $options['classes'] ) ) . '" ';
+			if ($options['for_swiper']) {
+				$img_html .= 'class="swiper-lazy ' . esc_attr(implode(' ', $options['classes'])) . '" ';
 			} else {
-				$img_html .= 'class="js-lazyload-image ' . esc_attr( implode( ' ', $options['classes'] ) ) . '" ';
-				$img_html .= 'src="' . esc_url( wp_get_attachment_image_url( $image_id, 'thumbnail' ) ) . '" ';
+				$img_html .= 'class="js-lazyload-image ' . esc_attr(implode(' ', $options['classes'])) . '" ';
+				$img_html .= 'src="' . esc_url(wp_get_attachment_image_url($image_id, 'thumbnail')) . '" ';
 			}
 		}
 
---
+++ includes/classes/class-ws-admin-controller.php
@@ -0,0 +1,252 @@
+<?php
+if (!defined('ABSPATH')) {
+	exit; // Exit if accessed directly.
+}
+/**
+ * Example class
+ */
+class WS_Admin_Controller
+{
+	/**
+	 * Example constructor.
+	 */
+	public function __construct()
+	{
+		$this->remove_actions();
+		$this->remove_filters();
+		$this->add_actions();
+		$this->add_filters();
+	}
+
+	/**
+	 * Add WordPress actions
+	 */
+	private function add_actions()
+	{
+		add_action('login_enqueue_scripts', [$this, 'ws_enqueue_admin_styles']);
+		add_action('admin_enqueue_scripts', [$this, 'ws_enqueue_admin_styles']);
+
+		add_action('login_header', [$this, 'include_custom_login_header_html']);
+		add_action('login_footer', [$this, 'include_custom_login_footer_html']);
+		add_action('login_head', [$this, 'ws_remove_loginshake']);
+
+
+		add_action('admin_init', [$this, 'add_site_options_menu_item']);
+		add_action('admin_init', [$this, 'hide_content_editor_from_options_page']);
+		add_action('admin_menu', [$this, 'hide_page_attributes_from_options_page']);
+
+		add_action('wp_before_admin_bar_render', [$this, 'add_admin_bar_button']);
+
+		add_action('wp_dashboard_setup', [$this, 'dashboard_ws_info']);
+		add_action('wp_dashboard_setup', [$this, 'dashboard_ws_rss']);
+	}
+
+	/**
+	 * Remove WordPress existing filters
+	 */
+	private function remove_filters()
+	{
+	}
+
+	/**
+	 * Remove WordPress existing actions
+	 */
+	private function remove_actions()
+	{
+		remove_action('login_head', 'wp_shake_js', 12);
+	}
+
+	/**
+	 * Add WordPress custom filters
+	 */
+	private function add_filters()
+	{
+		add_filter('login_headerurl', [$this, 'ws_override_login_logo_url']);
+		add_filter('parse_query', [$this, 'hide_options_page']);
+		add_filter('lostpassword_url', [$this, 'maybe_reset_login_url'], 10, 2);
+	}
+
+	/**
+	 * LOGIN
+	 *
+	 * Enqueues gulp generated admin-style.css file
+	 *
+	 * @return void
+	 */
+	public function ws_enqueue_admin_styles()
+	{
+		$manifest      = json_decode(file_get_contents(get_template_directory() . '/package.json', true));
+		$asset_version = $manifest->version;
+		wp_enqueue_style('ws-custom-login-style', get_stylesheet_directory_uri() . '/admin-style.css', [], $asset_version);
+	}
+
+	public function ws_remove_loginshake()
+	{
+		remove_action('login_head', 'wp_shake_js', 12);
+	}
+
+	public function include_custom_login_header_html()
+	{
+		include get_template_directory() . '/template-parts/admin/admin-login-header.php';
+	}
+
+	public function include_custom_login_footer_html()
+	{
+		include get_template_directory() . '/template-parts/admin/admin-login-footer.php';
+	}
+	/**
+	 * Override wp-login.php header logo URL.
+	 *
+	 * @param string $header_link
+	 *
+	 * @return string
+	 */
+	public function ws_override_login_logo_url($header_link)
+	{
+		return home_url();
+	}
+
+	/**
+	 * OPTIONS PAGE
+	 *
+	 * Adds the options page link to the admin menu.
+	 */
+	public function add_site_options_menu_item()
+	{
+		$options_page_id = ThemeSetup::get_theme_options_page_id();
+		if (!is_wp_error($options_page_id) && filter_var($options_page_id, FILTER_VALIDATE_INT)) {
+			add_menu_page(
+				'ws_site_options',
+				esc_html__('Site options'),
+				'read',
+				'post.php?post=' . $options_page_id . '&action=edit',
+				'',
+				'dashicons-admin-site',
+				12
+			);
+		}
+	}
+
+	/**
+	 * Hides the content editor from the options page admin UI.
+	 */
+	public function hide_content_editor_from_options_page()
+	{
+		$post_id = filter_input(INPUT_GET, 'post', FILTER_VALIDATE_INT);
+		if (!$post_id) {
+			$post_id = filter_input(INPUT_POST, 'post', FILTER_VALIDATE_INT);
+		}
+		if (!$post_id) {
+			return;
+		}
+		$options_page_id = ThemeSetup::get_theme_options_page_id();
+		if (!is_wp_error($options_page_id) && filter_var($options_page_id, FILTER_VALIDATE_INT)) {
+			if ($options_page_id === $post_id) {
+				remove_post_type_support('page', 'editor');
+				remove_post_type_support('page', 'thumbnail');
+			}
+		}
+	}
+	/**
+	 * Hides the page attributes from the options page admin UI.
+	 */
+	public function hide_page_attributes_from_options_page()
+	{
+		$post_id = filter_input(INPUT_GET, 'post', FILTER_VALIDATE_INT);
+		if (!$post_id) {
+			$post_id = filter_input(INPUT_POST, 'post', FILTER_VALIDATE_INT);
+		}
+		if (!$post_id) {
+			return;
+		}
+		$options_page_id = ThemeSetup::get_theme_options_page_id();
+		if (!is_wp_error($options_page_id) && filter_var($options_page_id, FILTER_VALIDATE_INT)) {
+			if ($options_page_id === $post_id) {
+				remove_meta_box('pageparentdiv', 'page', 'normal');
+			}
+		}
+	}
+	/**
+	 * Hides options page from everywhere.
+	 *
+	 * @param WP_Query $query WordPress query instance.
+	 */
+	public function hide_options_page($query)
+	{
+		global $pagenow, $post_type;
+		if (is_admin() && 'edit.php' === $pagenow && 'page' === $post_type) {
+			if (!filter_var($query->get('disable_for_options_page_id_query'), FILTER_VALIDATE_BOOLEAN)) {
+				$options_page_id = ThemeSetup::get_theme_options_page_id();
+				if (!is_wp_error($options_page_id) && filter_var($options_page_id, FILTER_VALIDATE_INT)) {
+					if ('ws-general-options-page-dummy-template.php' !== $query->query_vars['meta_value']) {
+						$post_not_in = $query->query_vars['post__not_in'];
+						if (!is_array($post_not_in)) {
+							$post_not_in = [];
+						}
+						$post_not_in[] = $options_page_id;
+
+						$query->query_vars['post__not_in'] = $post_not_in;
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * ADMIN BAR
+	 *
+	 * Adds custom admin bar button.
+	 */
+	public static function add_admin_bar_button()
+	{
+		get_template_part('template-parts/adminbar-button');
+	}
+
+	/**
+	 * ADMIN WIDGETS
+	 *
+	 *
+	 */
+	public function dashboard_ws_info()
+	{
+		global $wp_meta_boxes;
+
+		wp_add_dashboard_widget('dashboard_ws_info', 'Web Systems Info', [$this, 'ws_dashboard_info_function']);
+	}
+	public function ws_dashboard_info_function()
+	{
+		get_template_part('template-parts/admin/info-widget');
+	}
+	public function dashboard_ws_rss()
+	{
+		global $wp_meta_boxes;
+
+		wp_add_dashboard_widget('dashboard_ws_rss', __('Kasulikku lugemist', 'WebSystems'), [$this, 'ws_dashboard_rss_function']);
+	}
+	public function ws_dashboard_rss_function()
+	{
+		get_template_part('template-parts/admin/rss-widget');
+	}
+	/**
+	 * Filters the Lost Password URL.
+	 *
+	 * @since 2.8.0
+	 *
+	 * @param string $lostpassword_url The lost password page URL.
+	 * @param string $redirect         The path to redirect to on login.
+	 */
+	public function maybe_reset_login_url($lostpassword_url, $redirect)
+	{
+
+		if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')), true) && $GLOBALS['pagenow'] === 'wp-login.php') {
+			$args = array();
+			if (!empty($redirect)) {
+				$args['redirect_to'] = urlencode($redirect);
+			}
+
+			$lostpassword_url = add_query_arg($args, network_site_url('wp-login.php?action=lostpassword', 'login'));
+		}
+
+		return $lostpassword_url;
+	}
+}
